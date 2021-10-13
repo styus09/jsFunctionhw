946195b15de3d025233aa58439f55c28173e991b
Sean <styus09@gmail.com>
2021-10-13 01:29:22 -0400

    jsFunctionhw

---
+++ app.js
@@ -0,0 +1,22 @@
+function addTwo(params1, params2) {
+  console.log 
+    addTwo(10, 35)
+    addTwo(25, 40)
+}
+function subtractTwo(params1, params2) {
+  console.log [
+    subtractTwo(33, 9)
+    subtractTwo(26, 11)
+  ]
+}
+function multiplyTwo(params1, params2) {
+  console.log [
+    multiplyTwo(8, 6)
+    multiplyTwo(3, 12)
+  ]
+}
+function divideTwo(params1, params2) {
+  console.log 
+    divideTwo(60, 20)
+    divideTwo(90, 5)
+}
